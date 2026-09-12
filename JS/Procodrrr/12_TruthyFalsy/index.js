1e6e8004a2c4fb646bf8c67e7e58a040b831e648
Atlas <atlastoo012@gmail.com>
2026-09-12 22:29:59 +0530

    doing global context

---
+++ JS/Procodrrr/12_TruthyFalsy/index.js
@@ -0,0 +1,12 @@
+Boolean (12) //true
+Boolean(1)
+Boolean(0) //false
+Boolean("") //false //But if we put a space in the empty string then it would be true 
+Boolean("Hello") //true
+Boolean(null) //false
+Boolean(undefined) //false
+Boolean(NaN) //false
+Boolean([]) //true
+Boolean({}) //true
+ // except this all are 
+ console.log(-0>=0) //true
---
+++ JS/Procodrrr/L33_ExecutionContext /index.js
@@ -0,0 +1,20 @@
+//what we have already studied in the L20
+
+// Global Execution context
+
+   //a memory creation phase
+        //memory allocation for variables and functions
+   //b code execution 
+
+   
+// 2. Execution context stack
+
+debugger
+
+const username = "Ranjan"
+console.log(username) 
+
+
+function Sayhi(){
+    console.log("hi")
+}
